34be37b232653a65458d1703ade864f4dfb887c6
Pierre-Antoine Chéron <pa.ch3r0n@gmail.com>
2026-06-25 21:14:38 +0200

    do not remember last page at logout

--- frontend/lib/providers/auth_provider.dart
+++ frontend/lib/providers/auth_provider.dart
@@ -4,6 +4,8 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 import '../services/services.dart';
 import '../models/auth_state.dart';
 import '../models/invite_response.dart';
+import 'app_bar_config_provider.dart';
+import 'navigation_provider.dart';
 
 part 'auth_provider.g.dart';
 
@@ -25,6 +27,8 @@ class AuthNotifier extends _$AuthNotifier {
 
   /// Sets state to logged-out with success message (used after logout)
   void _setLoggedOutSuccessState() {
+    ref.invalidate(navigationSectionProvider);
+    ref.invalidate(appBarConfigProvider);
     state = const AuthState(
       isLoading: false,
       isAuthenticated: false,
